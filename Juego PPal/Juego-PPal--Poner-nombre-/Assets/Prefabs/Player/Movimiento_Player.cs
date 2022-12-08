6262a00f42844dfe47ef529eee864a16a768ca0e
juanchii-coder <71896282+juanchii-coder@users.noreply.github.com>
2022-12-08 16:51:38 -0300

    player movement
    cambios en el script de movimiento (100% hecho por nosotros, no fue ribado de ningun tutorial)

--- Juego PPal/Juego-PPal--Poner-nombre-/Assets/Prefabs/Player/Movimiento_Player.cs
+++ Juego PPal/Juego-PPal--Poner-nombre-/Assets/Prefabs/Player/Movimiento_Player.cs
@@ -1,84 +1,111 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+// Takes and handles input and movement for a player character
 public class Movimiento_Player : MonoBehaviour
 {
-    
+    public float moveSpeed = 1f;
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementFilter;
+    //public SwordAttack swordAttack;
+
+    Vector2 movementInput;
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D rb;
+    //Animator animator;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+    bool canMove = true;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    public bool canMoveLeft = true, canMoveRight = true, canMoveUp = true, canMoveDown = true;
+    private void FixedUpdate() {
+        if(canMove) {
+            // If movement input is not 0, try to move
+            if(movementInput != Vector2.zero){
+                
+                bool success = TryMove(movementInput);
 
-    
+                if(!success) {
+                    success = TryMove(new Vector2(movementInput.x, 0));
+                }
 
+                if(!success) {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+                
+                //animator.SetBool("isMoving", success);
+            }/* else {
+                animator.SetBool("isMoving", false);
+            }
 
-    void Update()
-    {
-        if (Input.GetKey("left") && canMoveLeft)
-        {
-
-            gameObject.transform.Translate(-3f * Time.deltaTime, 0, 0);
-            //canMoveRight = true;
-        }
-        if (Input.GetKey("right") && canMoveRight)
-        {
-            gameObject.transform.Translate(3f * Time.deltaTime, 0, 0);
-            //canMoveLeft = true;
-        }
-        if (Input.GetKey("up") && canMoveUp)
-        {
-            gameObject.transform.Translate(0, 3f * Time.deltaTime, 0);
-            //canMoveDown = true;
-        }
-        if (Input.GetKey("down") && canMoveDown)
-        {
-            gameObject.transform.Translate(0, -3f * Time.deltaTime, 0);
-            //canMoveUp = true;
+            // Set direction of sprite to movement direction
+            if(movementInput.x < 0) {
+                spriteRenderer.flipX = true;
+            } else if (movementInput.x > 0) {
+                spriteRenderer.flipX = false;
+            }*/
         }
     }
 
+    private bool TryMove(Vector2 direction) {
+        if(direction != Vector2.zero) {
+            // Check for potential collisions
+            int count = rb.Cast(
+                direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
+                movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
+                castCollisions, // List of collisions to store the found collisions into after the Cast is finished
+                moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        //Debug.Log("Collision");
-        if (other.collider.CompareTag("LeftLimit"))
-        {
-            canMoveLeft = false;
-            //Debug.Log("LeftLimit");
+            if(count == 0){
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            // Can't move if there's no direction to move in
+            return false;
         }
-        if (other.collider.CompareTag("RightLimit"))
-        {
-            canMoveRight = false;
-            //Debug.Log("RightLimit");
-        }
-        if (other.collider.CompareTag("UpLimit"))
-        {
-            canMoveUp = false;
-            //Debug.Log("UpLimit");
-        }
-        if (other.collider.CompareTag("DownLimit"))
-        {
-            canMoveDown = false;
-            //Debug.Log("DownLimit");
-        }
-
+        
     }
 
-    private void OnCollisionExit2D(Collision2D other)
-    {
-
-        if (other.collider.CompareTag("LeftLimit"))
-            canMoveLeft = true;
-        if (other.collider.CompareTag("RightLimit"))
-            canMoveRight = true;
-        if (other.collider.CompareTag("UpLimit"))
-            canMoveUp = true;
-        if (other.collider.CompareTag("DownLimit"))
-            canMoveDown = true;
+    void OnMove(InputValue movementValue) {
+        movementInput = movementValue.Get<Vector2>();
     }
 
+    /*void OnFire() {
+        animator.SetTrigger("swordAttack");
+    }*/
+
+    /*public void SwordAttack() {
+        LockMovement();
+
+        if(spriteRenderer.flipX == true){
+            swordAttack.AttackLeft();
+        } else {
+            swordAttack.AttackRight();
+        }
+    }
+
+    public void EndSwordAttack() {
+        UnlockMovement();
+        swordAttack.StopAttack();
+    }
+
+    public void LockMovement() {
+        canMove = false;
+    }
+
+    public void UnlockMovement() {
+        canMove = true;
+    }*/
 }
